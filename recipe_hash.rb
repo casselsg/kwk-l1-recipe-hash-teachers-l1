a7fdc8e93ca3c9b2830efc4b4bd0bf2b668e8eca
casselsg <casselsg19@bcdschool.org>
2018-07-12 16:04:39 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -26,17 +26,17 @@ perfect_10_recipe = {
 }
 
 
-#1. Write a method that simply returns (not puts) your perfect_10 hash
-def recipe_ingredients(perfect_10_recipe)
-  perfect_10_recipe
-end
+# #1. Write a method that simply returns (not puts) your perfect_10 hash
+# def recipe_ingredients(perfect_10_recipe)
+#   perfect_10_recipe
+# end
 
 
-#2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
-def amount_of_chocolate_chips(perfect_10_recipe)
-  perfect_10_recipe[:mini_dark_chocolate_chips]
+# #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
+# def amount_of_chocolate_chips(perfect_10_recipe)
+#   perfect_10_recipe[:mini_dark_chocolate_chips]
   
-end
+# end
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
 
@@ -46,16 +46,16 @@ def ingredients_and_amounts(perfect_10_recipe)
   end
 end
 
-#4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
-def ingredients(perfect_10_recipe)
-  perfect_10_recipe.each_key { |ingredient| puts ingredient }
-end
+# #4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
+# def ingredients(perfect_10_recipe)
+#   perfect_10_recipe.each_key { |ingredient| puts ingredient }
+# end
 
-#5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
-def amounts(perfect_10_recipe)
-end
+# #5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
+# def amounts(perfect_10_recipe)
+# end
 
-#6: Return the number of ingredients in the `perfect_10_recipe` hash.
-def number_ingredients(perfect_10_recipe)
-  # Use .size
-end
+# #6: Return the number of ingredients in the `perfect_10_recipe` hash.
+# def number_ingredients(perfect_10_recipe)
+#   # Use .size
+# end
