a8bd17ea16d9759c2a471c5344efa9f78f803230
casselsg <casselsg19@bcdschool.org>
2018-07-12 14:35:10 +0000

    automatically backed up by learn

--- nom.rb
+++ nom.rb
@@ -3,6 +3,15 @@
       "Monica" => "Neat Freak" ,
       "Joey" => "Jokester" ,
       "Chandler" => "Saracastic n00b" ,
-      "Phobe" => "Free Spirit"
-      "Rachel" => ""shopaholic
+      "Phobe" => "Free Spirit"  ,
+      "Rachel" => "shopaholic" ,
+      "Ross" => "Divorce" ,
      }
+     
+     character_list.each do |character_name, characteristic|
+       puts "character name : #{character_name}"
+       puts "characteristic : #{characteristic}"
+       puts ""
+     end
+     
+     
