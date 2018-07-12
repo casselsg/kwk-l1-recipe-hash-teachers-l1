c1c6d62961ca8d24899530d1ba1798969b477805
casselsg <casselsg19@bcdschool.org>
2018-07-12 14:09:07 +0000

    automatically backed up by learn

--- 1D.rb
+++ 1D.rb
@@ -1,8 +1,10 @@
 
-      one_direction = { "Zayn"one_direction = {"Zayn" => 25,
+
+one_direction = {"Zayn" => 25,
                  "Liam" => 24,
                  "Louis" => 26,
                  "Harry" => 24,
                  "Niall" => 24 
 }
 ​
+      
