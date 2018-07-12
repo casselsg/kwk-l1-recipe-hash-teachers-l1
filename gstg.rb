fe021966de90391e895428ffd5512d0093ec0138
casselsg <casselsg19@bcdschool.org>
2018-07-12 13:42:18 +0000

    Done.

---
+++ gstg.rb
@@ -0,0 +1,16 @@
+states = {"Massachusetts" => "MA",
+         "Wisconsin" => "WI",
+          "New Jersey" => "NJ",
+          "Colorado" => "CO"}
+​
+capitals = {"MA" => "Boston",
+           "WI" => "Madison",
+           "NJ" => "Trenton",
+            "CO" => "Denver"}
+
+
+puts "which state would you like to know the capital of?"
+ given = gets.chomp
+ 
+ puts states capitals[states]
+ 
