dedca68f863e2c959d7266efd77398d7a68ab0c6
casselsg <casselsg19@bcdschool.org>
2018-07-12 14:14:45 +0000

    automatically backed up by learn

--- 1D.rb
+++ 1D.rb
@@ -7,12 +7,12 @@ one_direction = {"Zayn" => 25,
                  "Niall" => 24 
 }
 ​
-      given = "Zayn"
+
+
+
+given = "Zayn"
 puts "Zayn,"
 puts [one_direction[given]]
-
-
-
 
 one_direction.each do |name, age|
   puts "#{name} is #{age} years old."
